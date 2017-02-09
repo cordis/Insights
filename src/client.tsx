faf70b11353d2305d4d2d187826b8648f07f4d91
cordis <cordis@game-mafia.ru>
2017-02-09 14:57:37 +0300

    r decoupling insights from the whole app

--- src/client.tsx
+++ src/client.tsx
@@ -2,8 +2,27 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as injectTapEventPlugin from "react-tap-event-plugin";
 
+import {AppBar} from "material-ui";
+import {getMuiTheme} from "material-ui/styles";
+import {blue500} from "material-ui/styles/colors";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
 import Insights from "./insights";
 
 injectTapEventPlugin();
 
-ReactDOM.render(<Insights />, document.getElementById("Insights"));
+let theme = getMuiTheme({
+    palette: {
+        accent1Color: blue500,
+        primary1Color: blue500
+    }
+});
+
+ReactDOM.render((
+    <MuiThemeProvider muiTheme={theme}>
+        <div>
+            <AppBar />
+            <Insights />
+        </div>
+    </MuiThemeProvider>
+), document.getElementById("App"));
--- src/insights.tsx
+++ src/insights.tsx
@@ -1,9 +1,5 @@
 import * as React from "react";
 
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import getMuiTheme from "material-ui/styles/getMuiTheme";
-import {blue500} from "material-ui/styles/colors";
-import {AppBar} from "material-ui";
 import {Tabs} from "material-ui";
 import {Tab} from "material-ui";
 
@@ -11,34 +7,16 @@ export interface InsightsProps {
 }
 
 export default class Insights extends React.Component<InsightsProps, {}> {
-    private theme: __MaterialUI.Styles.MuiTheme;
-
-    constructor() {
-        super();
-
-        this.theme = getMuiTheme({
-            palette: {
-                accent1Color: blue500,
-                primary1Color: blue500
-            }
-        });
-    }
-
     render() {
         return (
-            <MuiThemeProvider muiTheme={this.theme}>
-                <div>
-                    <AppBar />
-                    <Tabs>
-                        <Tab label="Overview"></Tab>
-                        <Tab label="Social"></Tab>
-                        <Tab label="Top Content"></Tab>
-                        <Tab label="Contributors"></Tab>
-                        <Tab label="Referrals"></Tab>
-                        <Tab label="Export"></Tab>
-                    </Tabs>
-                </div>
-            </MuiThemeProvider>
+            <Tabs>
+                <Tab label="Overview"></Tab>
+                <Tab label="Social"></Tab>
+                <Tab label="Top Content"></Tab>
+                <Tab label="Contributors"></Tab>
+                <Tab label="Referrals"></Tab>
+                <Tab label="Export"></Tab>
+            </Tabs>
         );
     }
 }
